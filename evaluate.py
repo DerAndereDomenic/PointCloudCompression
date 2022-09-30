373d74cc5e61177e4c9fdee59fb70a5da1fa47ed
Domenic Zingsheim <domeniczingsheim@gmail.com>
2022-09-30 11:47:35 +0200

    Add evaluation script

---
+++ evaluate.py
@@ -0,0 +1,38 @@
+import numpy as np
+from Common import *
+import os
+from scipy.spatial import cKDTree
+
+def compression_ratio():
+    file_size_uncompressed = os.path.getsize("model.bin")
+    print(f"Uncompressed filesize: {np.round(file_size_uncompressed/(1024**2),2)} MB")
+
+    file_size_compressed = 0
+    for path in os.listdir("output"):
+        file_size_compressed += os.path.getsize("output/" + path)
+    
+    print(f"Compressed filesize: {np.round(file_size_compressed/(1024**2),2)} MB")
+    print(f"Compression ratio: {np.round(file_size_compressed/file_size_uncompressed * 100, 2)} %")
+
+def symmetric_rmse():
+    pc_uncompressed = load_pointcloud("sample.xyz")
+    pc_compressed = load_pointcloud("decompressed.xyz")
+
+    P = pc_uncompressed.xyz
+    Q = pc_compressed.xyz
+
+    tree_P = cKDTree(P)
+    tree_Q = cKDTree(Q)
+
+    query_P,_ = tree_Q.query(P, k=1)
+    query_Q,_ = tree_P.query(Q, k=1)
+
+    MSE_PQ = np.mean(query_P**2)
+    MSE_QP = np.mean(query_Q**2)
+
+    RMSE_SNN = np.sqrt(0.5 * MSE_PQ + 0.5 * MSE_QP)
+    print(f"RMSE_NN: {RMSE_SNN}")
+
+if __name__ == "__main__":
+    compression_ratio()
+    symmetric_rmse()
